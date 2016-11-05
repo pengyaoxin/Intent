354c2471b63b168cd5384d2e56bf0770154f13a9
pengyaoxin <826550396@qq.com>
2016-11-05 21:20:43 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150738/intent/IntentDemo2.java
@@ -0,0 +1,29 @@
+package cn.edu.gdmec.s07150738.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by hello on 2016/11/5.
+ */
+
+public class IntentDemo2 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_intent_demo2);
+    }
+    public void upper(View v){
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        final String value = bundle.getString("value").toUpperCase();
+
+        Intent intentresult = new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+        finish();
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/s07150738/intent/IntentDemo3.java
@@ -0,0 +1,17 @@
+package cn.edu.gdmec.s07150738.intent;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hello on 2016/11/5.
+ */
+
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
